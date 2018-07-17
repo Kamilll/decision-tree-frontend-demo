a830830fe2944ebb1ecf933cfbb8ccc3d8e5c2de
Kamil Walerianczyk <kamil.walerianczyk@avaloq.com>
2018-07-17 15:08:23 +0200

    Add 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SegmentsComponent } from './segments/segments.component';
+import { SegmentDetailsComponent } from './segment-details/segment-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SegmentsComponent,
+    SegmentDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/segment-details/segment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segment-details',
+  templateUrl: './segment-details.component.html',
+  styleUrls: ['./segment-details.component.css']
+})
+export class SegmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
