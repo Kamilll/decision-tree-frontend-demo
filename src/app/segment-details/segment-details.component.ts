3cade8b3ed514a26f96ecae53c716c442b797758
Kamil Walerianczyk <kamil.walerianczyk@avaloq.com>
2018-07-18 17:37:39 +0200

    Simplify Segment Details Component

--- src/app/segment-details/segment-details.component.ts
+++ src/app/segment-details/segment-details.component.ts
@@ -1,36 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {DataSet} from 'vis';
+import {Component, Input, OnInit} from '@angular/core';
+import {Segment} from '../Segment';
 
 @Component({
   selector: 'app-segment-details',
-  templateUrl: './segment-details.component.html',
-  styleUrls: ['./segment-details.component.css']
+  template: `
+    <p>
+      segment-details works!
+    </p>
+    <p>{{segment.decisionTree}}</p>
+    <div [appGraphVis]="segment" style="height: 60vh; background-color: gray;"></div>
+  `
 })
 export class SegmentDetailsComponent implements OnInit {
 
-  graphData = {};
+  @Input() segment: Segment;
 
   constructor() {
   }
 
   ngOnInit() {
-    const nodes = new DataSet([
-      {id: 1, label: 'Node 1'},
-      {id: 2, label: 'Node 2'},
-      {id: 3, label: 'Node 3'},
-      {id: 4, label: 'Node 4'},
-      {id: 5, label: 'Node 5'}
-    ]);
-
-    const edges = new DataSet([
-      {from: 1, to: 3},
-      {from: 1, to: 2},
-      {from: 2, to: 4},
-      {from: 2, to: 5}
-    ]);
-
-    this.graphData[nodes] = nodes;
-    this.graphData[edges] = edges;
-    console.log('Loaded static data from SegmentDetailsCompo');
   }
 }
