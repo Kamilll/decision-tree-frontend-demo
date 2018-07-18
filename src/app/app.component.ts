17f32239e4b03285a911e3ffa7aeb0a75e77f07c
Kamil Walerianczyk <kamil.walerianczyk@avaloq.com>
2018-07-18 13:24:06 +0200

    Reduce number of app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <mat-toolbar color="primary">
+      <span>Test Test</span>
+    </mat-toolbar>
+    <div fxLayout="row">
+      <div fxFlex="200px">
+        <mat-nav-list>
+          <a mat-list-item href="#">Link 1</a>
+          <a mat-list-item href="#">Link 2</a>
+          <a mat-list-item href="#">Link 3</a>
+        </mat-nav-list>
+      </div>
+      <div fxFlex style="padding-top: 8px">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'stage-demo';
 }
