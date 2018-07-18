3d6724d020ba6180cd67fae7dc4337a5ecdddd8b
Kamil Walerianczyk <kamil.walerianczyk@avaloq.com>
2018-07-18 13:17:37 +0200

    Simple navigation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
   declarations: []
 })
 export class MaterialModule {
