f30061f620dc2efc57c6fd1e0d2d64d63a49d260
Kamil Walerianczyk <kamil.walerianczyk@avaloq.com>
2018-07-20 17:18:21 +0200

    Snack bar module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {MatButtonModule, MatIconModule, MatListModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTabsModule, MatListModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTabsModule, MatListModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTabsModule, MatListModule, MatSnackBarModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatTabsModule, MatListModule, MatSnackBarModule],
   declarations: []
 })
 export class MaterialModule {
--- src/app/segments/segments.component.ts
+++ src/app/segments/segments.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Segment} from '../segment';
 import {SegmentService} from '../segment.service';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-segments',
@@ -17,8 +18,13 @@ import {SegmentService} from '../segment.service';
 export class SegmentsComponent implements OnInit {
 
   segments: Segment[];
+  error: any;
 
-  constructor(private segmentService: SegmentService) {
+  constructor(private segmentService: SegmentService, private snackBar: MatSnackBar) {
+  }
+
+  openSnackbar() {
+    this.snackBar.open('Error', null, {duration: 2000});
   }
 
   ngOnInit() {
@@ -26,7 +32,11 @@ export class SegmentsComponent implements OnInit {
   }
 
   getSegments(): void {
-    this.segmentService.getSegments().subscribe(segments => this.segments = segments);
+    this.segmentService.getSegments().subscribe(
+      segments => this.segments = segments,
+      (error) => {
+        this.error = error;
+        this.openSnackbar();
+      });
   }
-
 }
